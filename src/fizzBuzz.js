077d23dd907026745d054a36819537b40ed6dea6
Angelica <Angelica137@users.noreply.github.com>
2020-04-15 13:47:38 +0100

    tdd fizz when 3, green

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -11,3 +11,9 @@ Fizzbuzz.prototype.isDivisibleByFifteen = function(number) {
 Fizzbuzz.prototype.isDivisibleByFive = function(number) {
   return this._isDivisibleBy(number, 5);
 };
+
+Fizzbuzz.prototype.says = function(number) {
+  if (this.isDivisibleByThree(number)) {
+    return "Fizz";
+  }
+};
