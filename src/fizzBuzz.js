29c256b2e067477255afc567dd156d739200986e
Angelica <Angelica137@users.noreply.github.com>
2020-04-15 13:36:41 +0100

    is diviisble by 5, green

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -7,3 +7,7 @@ Fizzbuzz.prototype.isDivisibleByThree = function(number) {
 Fizzbuzz.prototype.isDivisibleByFifteen = function(number) {
   return this._isDivisibleBy(number, 15);
 };
+
+Fizzbuzz.prototype.isDivisibleByFive = function(number) {
+  return this._isDivisibleBy(number, 5);
+};
