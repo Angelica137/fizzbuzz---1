5302e7c974cb563dce19f1df0a1ba22debbc060e
Angelica <Angelica137@users.noreply.github.com>
2020-04-15 12:54:00 +0100

    fizzbuzz knows when  number is divisible by three amend  first test to define class Fizzbuzz

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -2,7 +2,7 @@ describe('Fizzbuzz', function() {
   var fizzbuzz;
   describe('knows when a number is', function() {
     it('divisible by 3', function() {
-      fizzbuzz = new fizzbuzz();
+      fizzbuzz = new Fizzbuzz();
       expect(isDivisibleByThree(3).toBe(true));
     });
   });
