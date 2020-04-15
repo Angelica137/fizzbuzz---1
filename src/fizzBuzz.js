b0ea5a695af8b5ced07220a2d107cc964d3ade7e
Angelica <Angelica137@users.noreply.github.com>
2020-04-15 13:33:51 +0100

    tdd is divisible by 15, green

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -3,3 +3,7 @@ var Fizzbuzz = function(){};
 Fizzbuzz.prototype.isDivisibleByThree = function(number) {
   return this._isDivisibleBy(number, 3);
 };
+
+Fizzbuzz.prototype.isDivisibleByFifteen = function(number) {
+  return this._isDivisibleBy(number, 15);
+};
