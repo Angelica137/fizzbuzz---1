7279c9eff63c90dae0f63639e9c365c494564397
Angelica <Angelica137@users.noreply.github.com>
2020-04-15 13:00:42 +0100

    knows when a number is divisible by 3, tdd green

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -3,7 +3,7 @@ describe('Fizzbuzz', function() {
   describe('knows when a number is', function() {
     it('divisible by 3', function() {
       fizzbuzz = new Fizzbuzz();
-      expect(isDivisibleByThree(3).toBe(true));
+      expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
     });
   });
 });
